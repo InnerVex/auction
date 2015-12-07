d6040d1be86dd83cf8f1902fc5067ae492902224
InnerVex <mutalisk22@mail.ru>
2015-12-07 13:21:18 +0600

    m

--- auctionApp/models.py
+++ auctionApp/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 
 class Trader(models.Model):
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(User, default=User.objects.create_user('delete_me', 'delete_me@delete_me.ru', 'delete_me'))
     information = models.TextField(blank=True)
 
     def __str__(self):
@@ -12,15 +12,15 @@ class Trader(models.Model):
 
 
 class Buyer(models.Model):
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(User, default=User.objects.create_user('delete_me', 'delete_me@delete_me.ru', 'delete_me'))
 
     def __str__(self):
         return str(self.user)
 
 
 class Lot(models.Model):
-    trader = models.ForeignKey(Trader)
-    name = models.CharField(max_length=80)
+    trader = models.ForeignKey(Trader, default=Trader.objects.create())
+    name = models.CharField(max_length=80, default='default_name')
     starting_bid = models.FloatField(default=0)
     buyoff_price = models.FloatField(default=0)
     expires = models.DateTimeField('the day the bidding ends', default=timezone.now())
@@ -40,8 +40,8 @@ class Lot(models.Model):
 
 
 class Bid(models.Model):
-    lot = models.ForeignKey(Lot)
-    buyer = models.ForeignKey(Buyer)
+    lot = models.ForeignKey(Lot, default=Lot.objects.create())
+    buyer = models.ForeignKey(Buyer, default=Buyer.objects.create())
     price = models.FloatField(default=0)
     date = models.DateTimeField('the day the bid was made', default=timezone.now())
 
