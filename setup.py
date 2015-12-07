d084f774b6ecc1838e3815d8256b0db7327039a8
InnerVex <mutalisk22@mail.ru>
2015-12-07 12:38:22 +0600

    +setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+import os
+from setuptools import setup
+
+setup(
+    name = "auction",
+    version = "0.0.1",
+    author = "Kaziev_Gelginberg_Volzhanina_Vorotov",
+    author_email = "mutalisk22@mail.ru",
+    description = ("8VM51 legacy."),
+    keywords = "example documentation tutorial",
+    packages=['an_example_pypi_project', 'tests'],
+)
